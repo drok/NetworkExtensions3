7cb91767dd25553313dcdb6df88cf25911e92f14
Radu Hociung <radu.csmods@ohmi.org>
2022-01-24 13:48:46 -0500

    Rename project to NetworkExtensions 3

---
+++ NetworkExtensions3/Mod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ColossalFramework;
+using ColossalFramework.Plugins;
+using Transit.Framework;
+using Transit.Framework.Mod;
+using Transit.Framework.Modularity;
+
+namespace NetworkExtensions
+{
+    public sealed partial class Mod : TransitModBase
+    {
+        public override string Name
+        {
+            get { return "Network Extensions 3"; }
+        }
+
+        public override string Description
+        {
+            get { return "All roads lead to Rome."; }
+        }
+
+        public override string Version
+        {
+            get { return "1.0.0"; }
+        }
+
+    }
+}
